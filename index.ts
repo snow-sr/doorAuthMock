a311a165f0a3b945e62e3526a873d7dd5c7d34b5
snow-sr <joaofelipi877@gmail.com>
2023-11-07 01:31:25 -0300

    Add RFID and User routes

--- index.ts
+++ index.ts
@@ -1,26 +1,22 @@
-import { checkRfid } from "./db.js";
 import express from "express";
 import bp from "body-parser";
+import rfidRoutes from "./rfid/rfid.js";
+import userRoutes from "./user/user.js";
 const port: number = 8087;
+
 
 const app = express();
 
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
+app.use("/rfid", rfidRoutes);
+app.use("/user", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Basic door api");
 });
 
-app.post("/door", async (req: express.Request, res) => {
-  const Rfid: string = req.body.rfid;
-  const check = await checkRfid(Rfid);
-  if (check) {
-    res.send("Door open");
-  } else {
-    res.status(401).send("Door closed");
-  }
-});
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);

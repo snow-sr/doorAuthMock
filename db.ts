e2b3ba1592171f5cd50b03fbdb66056b32dd3b4d
snow-sr <joaofelipi877@gmail.com>
2023-11-07 01:31:15 -0300

    Add CRUD functions for RFID and User models

--- db.ts
+++ db.ts
@@ -17,3 +17,96 @@ export async function checkRfid(rfid: string) {
     return false;
   }
 }
+
+export async function createRfid(rfid: string) {
+  const rfidTag = await prisma.rfidTag.create({
+    data: {
+      rfid: rfid,
+      lastTimeUsed: new Date(),
+    },
+  });
+
+  prisma.$disconnect();
+
+  return rfidTag;
+}
+
+export async function deleteRfid(rfid: string) {
+  const rfidTag = await prisma.rfidTag.delete({
+    where: {
+      rfid: rfid,
+    },
+  });
+
+  prisma.$disconnect();
+
+  return rfidTag;
+}
+
+export async function listAllRfids() {
+  const rfids = await prisma.rfidTag.findMany();
+  return rfids;
+}
+
+export async function listAllUser() {
+  const users = await prisma.user.findMany();
+
+  prisma.$disconnect();
+
+  return users;
+}
+
+
+// model user {
+//   id        Int      @id @unique @default(autoincrement())
+//   name      String
+//   email     String   @unique
+//   createdAt DateTime @default(now())
+//   rfid      rfidTag?
+// }
+
+
+export async function createUser(name: string, email: string) {
+  const user = await prisma.user.create({
+    data: {
+      name: name,
+      email: email,
+    },
+  });
+
+  prisma.$disconnect();
+
+  return user;
+}
+
+export async function assignRfidToUser(rfid: string, email: string) {
+  const user = await prisma.user.update({
+    where: {
+      email: email,
+    },
+    data: {
+      rfid: {
+        connect: {
+          rfid: rfid,
+        },
+      },
+    },
+  });
+
+  const rfidTag = await prisma.rfidTag.update({
+    where: {
+      rfid: rfid,
+    },
+    data: {
+      user: {
+        connect: {
+          email: email,
+        },
+      },
+    },
+  });
+
+  prisma.$disconnect();
+
+  return user;
+}

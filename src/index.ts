36f4f856ed7bd66f4dacda71c430451f26360deb
snow-sr <joaofelipi877@gmail.com>
2023-11-22 11:43:19 -0300

    Add cors package for cross-origin resource sharing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import door from "./routes/door/door_functions.js";
 import user from "./routes/user/user_functions.js";
 import express from "express";
+import cors from "cors";
 import bp from "body-parser";
 const port: number = 8087;
 
@@ -8,8 +9,10 @@ const app = express();
 
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
+app.use(cors());
 app.use(door);
 app.use(user);
+
 
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send("System working");

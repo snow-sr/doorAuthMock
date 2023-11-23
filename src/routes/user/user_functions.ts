1048033abbbbdeab995625db3d01559a6cfb28aa
snow-sr <joaofelipi877@gmail.com>
2023-11-23 10:20:46 -0300

    Refactor user routes and functions

--- src/routes/user/user_functions.ts
+++ src/routes/user/user_functions.ts
@@ -1,7 +1,37 @@
 import express from "express";
+import { listAllUser, createUser, isUserSynced } from "../../db/db.js";
 const router = express.Router();
 
-router.post("/users/", ( res: express.Response) => {
-    res.send("Users");
-})
+router.post("/create/", (req: express.Request, res: express.Response) => {
+  const name: string = req.body.name;
+  const email: string = req.body.email;
+
+  createUser(name, email);
+
+  res.send("User created");
+});
+
+router.get("/users/", async (req, res) => {
+  const users = await listAllUser();
+  res.send(users);
+});
+
+// router.post("/assign/", async (req: express.Request, res: express.Response) => {
+//   const rfid: string = req.body.rfid;
+//   const email: string = req.body.email;
+
+//   await assignRfidToUser(rfid, email);
+
+//   res.send(`${rfid} assigned to ${email}`);
+// });
+
+router.post("/sync/", async (req: express.Request, res: express.Response) => {
+  const name: string = req.body.name;
+  const firebase_id: string = req.body.firebase_id;
+
+  await isUserSynced(firebase_id, name);
+
+  res.send(`${name} synced`);
+});
+
 export default router;

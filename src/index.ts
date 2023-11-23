ae63c740c36e5e5d4282414958b4eb4aaa3e1a7a
snow-sr <joaofelipi877@gmail.com>
2023-11-23 10:26:33 -0300

    Refactor CORS configuration

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,8 @@ const app = express();
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
 // allow all origins
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
+
+app.use(cors()); // Allow all origins
 app.use(door);
 app.use(user);
 

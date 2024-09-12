839ccde30c6a7f98dd99c28e26005dbb583069d1
jonatassilvaperaza@gmail.com <jonatassilvaperaza@gmail.com>
2024-09-12 15:03:11 -0300

    Update database configuration and add CORS support

--- dist/index.js
+++ dist/index.js
@@ -3,17 +3,37 @@ import user from "./routes/user/user_functions.js";
 import express from "express";
 import cors from "cors";
 import bp from "body-parser";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
 const port = 8087;
 const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    },
+});
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
-app.use(cors());
+// allow all origins
+app.use(cors()); // Allow all origins
 app.use(door);
 app.use(user);
 app.get("/", (req, res) => {
+    console.log(req.ip);
     res.send("System working");
 });
-app.listen(port, () => {
+// socket
+io.on("connection", (socket) => {
+    console.log("a user connected");
+    socket.on("disconnect", () => {
+        console.log("user disconnected", socket.id);
+    });
+});
+server.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+export const updateFront = () => {
+    io.emit("updateFront", true);
+};
 //# sourceMappingURL=index.js.map
--- src/db/db.ts
+++ src/db/db.ts
@@ -2,141 +2,149 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function checkRfid(rfid: string) {
-  const Exists = await prisma.rfidTag.findUnique({
-    where: {
-      rfid: rfid,
-    },
-  });
-
-  if (Exists) {
-    await prisma.rfidTag.update({
+  try {
+    const Exists = await prisma.rfidTag.findUnique({
       where: {
         rfid: rfid,
       },
-      data: {
-        last_time_used: new Date(),
-      },
     });
 
-    prisma.$disconnect();
+    if (Exists) {
+      await prisma.rfidTag.update({
+        where: {
+          rfid: rfid,
+        },
+        data: {
+          last_time_used: new Date(),
+        },
+      });
 
-    if (Exists.valid) {
-      console.log("destrancado");
-      return true;
-    } else {
-      console.log("trancado");
+      if (Exists.valid) {
+        console.log("destrancado");
+        return true;
+      } else {
+        console.log(Exists);
+        console.log("Minha Pomba Gira Rainha das 7 Encruzilhadas");
+        console.log(Exists.valid);
+        console.log("trancado");
+        return false;
+      }
+    }
+
+    console.log("ESSA PORRA EXISTE? ", Exists);
+
+    if (!Exists) {
+      console.log("Creating new rfid");
+      await prisma.rfidTag.create({
+        data: {
+          rfid: rfid,
+        },
+      });
+
+      console.log("Created");
+
       return false;
     }
-  }
-
-  console.log("ESSA PORRA EXISTE? ", Exists);
-
-  if (!Exists) {
-    console.log("Creating new rfid");
-    await prisma.rfidTag.create({
-      data: {
-        rfid: rfid,
-      },
-    });
-
-    console.log("Created");
-
-    return false;
+  } finally {
+    await prisma.$disconnect();
   }
 }
 
 export async function deleteRfid(rfid: string) {
-  const rfidTag = await prisma.rfidTag.delete({
-    where: {
-      rfid: rfid,
-    },
-  });
+  try {
+    const rfidTag = await prisma.rfidTag.delete({
+      where: {
+        rfid: rfid,
+      },
+    });
 
-  prisma.$disconnect();
-
-  return rfidTag;
+    return rfidTag;
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
 export async function listAllRfids() {
-  const rfids = await prisma.rfidTag.findMany();
-  return rfids;
+  try {
+    const rfids = await prisma.rfidTag.findMany();
+    return rfids;
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
 export async function listAllUser() {
-  const users = await prisma.user.findMany();
-
-  prisma.$disconnect();
-
-  return users;
+  try {
+    const users = await prisma.user.findMany();
+    return users;
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
-// model user {
-//   id        Int      @id @unique @default(autoincrement())
-//   name      String
-//   email     String   @unique
-//   createdAt DateTime @default(now())
-//   rfid      rfidTag?
-// }
-
 export async function createUser(name: string, firebase_id: string) {
-  const user = await prisma.user.create({
-    data: {
-      firebase_id: firebase_id,
-      name: name,
-    },
-  });
+  try {
+    const user = await prisma.user.create({
+      data: {
+        firebase_id: firebase_id,
+        name: name,
+      },
+    });
 
-  prisma.$disconnect();
-
-  return user;
+    return user;
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
 export async function assignRfidToUser(rfid: string, id: number) {
-  const user = await prisma.user.update({
-    where: {
-      id: id,
-    },
-    data: {
-      tags_owned: {
-        connect: {
-          rfid: rfid,
+  try {
+    const user = await prisma.user.update({
+      where: {
+        id: id,
+      },
+      data: {
+        tags_owned: {
+          connect: {
+            rfid: rfid,
+          },
         },
       },
-    },
-  });
+    });
 
-  const rfidTag = await prisma.rfidTag.update({
-    where: {
-      rfid: rfid,
-    },
-    data: {
-      valid: true,
-      user: {
-        connect: {
-          id: id,
+    const rfidTag = await prisma.rfidTag.update({
+      where: {
+        rfid: rfid,
+      },
+      data: {
+        valid: true,
+        user: {
+          connect: {
+            id: id,
+          },
         },
       },
-    },
-  });
+    });
 
-  prisma.$disconnect();
-
-  return user;
+    return user;
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
 export async function isUserSynced(firebaseUserID: string, name: string) {
-  const user = await prisma.user.findUnique({
-    where: {
-      firebase_id: firebaseUserID,
-    },
-  });
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        firebase_id: firebaseUserID,
+      },
+    });
 
-  if (user) {
-    return true;
-  } else {
-    console.log("Creating new user");
+    if (user) {
+      return true;
+    } else {
+      console.log("Creating new user");
 
-    try {
       await prisma.user.create({
         data: {
           firebase_id: firebaseUserID,
@@ -145,9 +153,11 @@ export async function isUserSynced(firebaseUserID: string, name: string) {
       });
       console.log("User created");
       return true;
-    } catch {
-      console.log("Error creating user");
-      return false;
     }
+  } catch {
+    console.log("Error creating user");
+    return false;
+  } finally {
+    await prisma.$disconnect();
   }
 }
